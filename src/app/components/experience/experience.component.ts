596d426639d0d1378084dfd74e7817d4618287cb
Jesus E Castillo <jesuseduaardo@gmail.com>
2020-06-26 11:17:32 -0300

    Se modifica el area de certificaciones

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExperienceService, Experience } from '../../services/experience.service';
 import { EducationService, Education } from '../../services/education.service';
 import { HabilitiesService, Hability } from '../../services/habilities.service';
+import { CertificationsService, Certification } from '../../services/certifications.service';
 
 @Component({
   selector: 'app-experience',
@@ -14,18 +15,20 @@ export class ExperienceComponent implements OnInit {
 
   experiences:Experience[] = [];
   education:Education[] = [];
+  certifications:Certification[]=[];
   habilities:Hability[] = [];
 
   constructor(
     private _experienceService:ExperienceService, 
     private _educationService:EducationService,
-    private _habilitiesService:HabilitiesService) { }
+    private _habilitiesService:HabilitiesService,
+    private _certificationService:CertificationsService) { }
 
   ngOnInit() {
     this.experiences = this._experienceService.getExperience();
     this.education = this._educationService.getEducation();
     this.habilities = this._habilitiesService.getHabilities();
-    console.log(this.habilities);
+    this.certifications = this._certificationService.getCertifications();
   }
 
 }
--- src/app/services/education.service.ts
+++ src/app/services/education.service.ts
@@ -12,48 +12,6 @@ export class EducationService {
       certificacionUrl:"https://drive.google.com/open?id=0B_kPEZBn5CtldlYzTzBLOVpmRU0",
       duracion:0
     },
-    {
-      titulo:"Angular (Basic) ",
-      institucion:"HackerRank",
-      fechaInicio:"2020-06-01",
-      fechaFin:"2020-06-01",
-      certificacionUrl:"https://www.hackerrank.com/certificates/bddba66b1683",
-      duracion:0
-      
-    },
-    {
-      titulo:"React (Basic) ",
-      institucion:"HackerRank",
-      fechaInicio:"2020-06-01",
-      fechaFin:"2020-06-01",
-      certificacionUrl:"https://www.hackerrank.com/certificates/86772c3ab8a4",
-      duracion:0
-      
-    },
-    {
-      titulo:"JavaScript Algorithms and Data Structures",
-      institucion:"FreeCodeCamp.org",
-      fechaInicio:"2020-05-01",
-      fechaFin:"2020-05-01",
-      certificacionUrl:"https://www.freecodecamp.org/certification/jesuseduaardo/javascript-algorithms-and-data-structures",
-      duracion:300
-    },
-    {
-      titulo:"Responsive Web Desing",
-      institucion:"FreeCodeCamp.org",
-      fechaInicio:"2018-01-01",
-      fechaFin:"2018-08-01",
-      certificacionUrl:"https://www.freecodecamp.org/certification/jesuseduaardo/responsive-web-design",
-      duracion:300
-    },
-    {
-      titulo:"ReactJS",
-      institucion:"Educacion IT",
-      fechaInicio:"2018-07-25",
-      fechaFin:"2018-08-25",
-      certificacionUrl:"https://alumni.education/user/diploma/codigo_diplo/191544/id_diploma/32403NI2R999E98J4314U5351VX4J4NF/",
-      duracion:15
-    }
   ];
 
   constructor() { }
