8b56cd19a33cf4c5d1e0d0e31cc193b2c1296539
Jesus E Castillo <jesuseduaardo@gmail.com>
2024-11-06 15:15:48 -0300

    Se ajusta comportamiento scroll y se agrega animacion a footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  currentYear:number;
+  currentYear: number;
 
   constructor() { }
 
   ngOnInit(): void {
     this.currentYear = new Date().getFullYear();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    const docHeight = document.documentElement.scrollHeight;
+    const winHeight = window.innerHeight;
+    const scrollTop = window.scrollY;
+    const socialIcons = document.querySelector('.social-icons');
+    if (scrollTop + winHeight >= docHeight) {
+      socialIcons!.classList.add('get-attention');
+    } else {
+      socialIcons!.classList.remove('get-attention');
+    }
   }
 
 }
